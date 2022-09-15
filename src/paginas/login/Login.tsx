814525905ba5561ccae11c796d6c2f9ea4dcfe23
Jonathan <jonathancqaa@gmail.com>
2022-09-15 11:20:00 -0300

    @jonathan [Task: Comunicação do login com o Back-End] {15/09/2022 11:19}

--- src/paginas/login/Login.tsx
+++ src/paginas/login/Login.tsx
@@ -4,17 +4,16 @@ import { Link, useNavigate } from 'react-router-dom';
 import { Box } from '@mui/material';
 import { login } from '../../services/Services';
 import UserLogin from '../../models/Autentication';
+import useLocalStorage from 'react-use-localstorage';
 import './Login.css';
 
 function Login(){
  
     let navigate= useNavigate();
-    const [token, setToken] = useState();
+    const [token, setToken] = useLocalStorage('token');
 
     const [userLogin, setUserLogin] = useState<UserLogin>(
         {
-            id: 0,
-            usuario: "",
             email: "",
             senha: "",
             token: ""
@@ -29,6 +28,12 @@ function Login(){
         })
     }
 
+    useEffect(()=>{
+        if(token !== ''){
+            navigate('/produtos')
+        }
+    })
+
 
 
     async function onSubmit(e: ChangeEvent<HTMLFormElement>){
@@ -36,7 +41,7 @@ function Login(){
         e.preventDefault();
 
         try {
-            await login(`/usuarios/logar`, userLogin, setToken)
+            await login(`/api/Usuarios/logar`, userLogin, setToken)
             
             alert('Usuario logado com sucesso!');
         } catch (error) {
@@ -51,7 +56,7 @@ function Login(){
                 <Box paddingX={20}>
                     <form onSubmit={onSubmit}>
                         <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='textos1'>Entrar</Typography>
-                        <TextField value={userLogin.usuario} onChange={(e:ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='usuario' label='Usuário' variant='outlined' name='usuario' margin='normal' fullWidth />
+                        <TextField value={userLogin.email} onChange={(e:ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='email' label='E-mail' variant='outlined' name='email' margin='normal' fullWidth />
                         <TextField value={userLogin.senha} onChange={(e:ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='senha' label='Senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
                         <Box marginTop={2} textAlign='center'>
                             <Button type='submit' variant='contained' color='primary'>
